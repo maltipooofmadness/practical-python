6baeae7b2c1c13d69787c30291acc8bf50216101
maltipooofmadness <yemshu+github@proton.me>
2024-07-31 14:10:22 +0200

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -4,10 +4,10 @@
 
 HEIGHT = 100 # Height of the drop (meters)
 Q_BOUNCE_BACK = 3/5 # Distance traveled upon bouncing back (coefficient)
-BOUNCE_COUNT = 1
+BOUNCE_COUNT = 0
 
-while i in range(10):
+while BOUNCE_COUNT in range(10):
     HEIGHT = Q_BOUNCE_BACK * HEIGHT
-    print(BOUNCE_COUNT, HEIGHT)
     BOUNCE_COUNT = BOUNCE_COUNT + 1
+    print(BOUNCE_COUNT, round(HEIGHT, 4))
 
