e755d0a5a5b8c628db62aa63262069c27c8fa922
maltipooofmadness <yemshu+github@proton.me>
2024-08-05 15:54:06 +0200

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -4,7 +4,7 @@
 
 with open('portfolio.csv','rt') as file:
     headers = next(file)
-    total_cost = 0
+    total_cost = 0.0
     for line in file:
         row = line.split(',')
         total_cost = total_cost + int(row[1]) * float(row[2])
