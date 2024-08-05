5583006f7ff03b3013e5e95609a8ad9fbc8eb2d3
maltipooofmadness <yemshu+github@proton.me>
2024-08-05 15:52:49 +0200

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+
+with open('portfolio.csv','rt') as file:
+    headers = next(file)
+    total_cost = 0
+    for line in file:
+        row = line.split(',')
+        total_cost = total_cost + int(row[1]) * float(row[2])
+        
+    print('Total cost to purchase the portfolio:', total_cost)
