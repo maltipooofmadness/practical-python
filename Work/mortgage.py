bad9725e90b86a0a33efacf0d5369a96c015bd51
maltipooofmadness <yemshu+github@proton.me>
2024-08-05 12:18:10 +0200

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -150,3 +150,41 @@ while PRINCIPAL > 0:
         print(MONTHS_TOTAL, round(TOTAL_PAID,2), round(PRINCIPAL,2))
 
 print("Total paid:", round(TOTAL_PAID,2), "over", MONTHS_TOTAL, "months...")
+
+#
+# Exercise 1.17
+#
+
+PRINCIPAL = 500000.00
+RATE = .05
+PAYMENT = 2684.11
+EXTRA_PAYMENT_START = 0
+EXTRA_PAYMENT_END = 0
+EXTRA_PAYMENT = 0
+EXTRA_PAYMENT_START = int(
+    input("Which month would you like to start your extra payments? "))
+EXTRA_PAYMENT_END = int(
+    input("Which month wouold you like to finish your extra payments? "))
+EXTRA_PAYMENT = float(
+    input("How much extra would you like to pay per month? "))
+TOTAL_PAID = 0.0
+MONTHS_TOTAL = 0
+
+while PRINCIPAL > 0:
+    MONTHS_TOTAL = MONTHS_TOTAL + 1
+    if MONTHS_TOTAL == EXTRA_PAYMENT_START:
+        while MONTHS_TOTAL <= EXTRA_PAYMENT_END:
+            PRINCIPAL = PRINCIPAL * (1 + RATE/12) - (PAYMENT + EXTRA_PAYMENT)
+            TOTAL_PAID = TOTAL_PAID + PAYMENT + EXTRA_PAYMENT
+            # print(MONTHS_TOTAL, round(TOTAL_PAID,2), round(PRINCIPAL,2), 
+            #      "- Extra payment month")
+            print(f'{MONTHS_TOTAL} {TOTAL_PAID:0.2f} {PRINCIPAL:0.2f}'
+                  ' - Extra payment month')
+            MONTHS_TOTAL = MONTHS_TOTAL + 1
+    PRINCIPAL = PRINCIPAL * (1 + RATE/12) - PAYMENT
+    TOTAL_PAID = TOTAL_PAID + PAYMENT
+    # print(MONTHS_TOTAL, round(TOTAL_PAID,2), round(PRINCIPAL,2))
+    print(f'{MONTHS_TOTAL} {TOTAL_PAID:0.2f} {PRINCIPAL:0.2f}')
+
+# print("Total paid:", round(TOTAL_PAID,2), "over", MONTHS_TOTAL, "months...")
+print(f'Total paid: {TOTAL_PAID:0.2f} over {MONTHS_TOTAL} months')
