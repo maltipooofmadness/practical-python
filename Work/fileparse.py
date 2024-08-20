369abe5cbe2e59ab16bd417e9bcd685592b92ebd
maltipooofmadness <yemshu+github@proton.me>
2024-08-20 19:34:26 +0200

    Update fileparse.py

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,16 +1,15 @@
-# fileparse.py
-#
-# Exercise 3.3
+####
+# Exercises 3.11 .. 3.17
+####
 
-####
-# Exercise 3.11
-####
 
 import csv
 import os
-os.chdir('C:\\Users\\90036ysh\\Dropbox\\Postdoc\\py')
+#os.chdir('C:\\Users\\90036ysh\\Dropbox\\Postdoc\\py')
+os.chdir('D:\\Dropbox\\Postdoc\\py')
 
-def parse_csv(filename,
+
+def parse_csv(lines,
               select=None,
               types=None,
               hasheaders=True,
@@ -20,51 +19,55 @@ def parse_csv(filename,
     Parse a csv file into a list of records (dictionaries)
 
     '''
+    if type(lines) == str:
+        raise RuntimeError('Please, open file first and then call .parse_csv')
+    
     if select and not hasheaders:
         raise RuntimeError('select argument requires column headers')
         
-    with open(filename) as f:
-        rows = csv.reader(f, delimiter=delimiter)
+    
+    rows = csv.reader(lines, delimiter=delimiter)
+   
         
-        # Determining whether the file has headers or not
-        if hasheaders:
-        # Read the file headers
-            headers = next(rows)
-        else:
-            headers = None
+    # Determining whether the file has headers or not
+    if hasheaders:
+    # Read the file headers
+        headers = next(rows)
+    else:
+        headers = None
         
-        # If specific columns have been selected, make indices for filtering
-        if select:
-            indices = [ headers.index(colname) for colname in select ]
-            headers = select
+    # If specific columns have been selected, make indices for filtering
+    if select:
+        indices = [ headers.index(colname) for colname in select ]
+        headers = select
             
-        records = []
-        for row_n,row in enumerate(rows,start=1):
-            if not row: # Skip rows with no data
+    records = []
+    for row_n,row in enumerate(rows,start=1):
+        if not row: # Skip rows with no data
+            continue
+            
+        # If specific columns were selected, pick them out
+        if select:
+            row = [ row[index] for index in indices ]
+             
+        # Convert select columns if specific types were selected
+        if types:
+            try:
+                row = [ funct(val) for funct,val in zip(types,row) ]
+            except ValueError as e:
+                if not silence_errors:
+                    print(f'Row {row_n}: Couldn\'t convert {row}')
+                    print(f'Row {row_n}: Reason: {e}')
                 continue
             
-            # If specific columns were selected, pick them out
-            if select:
-                row = [ row[index] for index in indices ]
-             
-            # Convert select columns if specific types were selected
-            if types:
-                try:
-                    row = [ funct(val) for funct,val in zip(types,row) ]
-                except ValueError as e:
-                    if not silence_errors:
-                        print(f'Row {row_n}: Couldn\'t convert {row}')
-                        print(f'Row {row_n}: Reason: {e}')
-                    continue
+        if headers:
+        # Make dictionaries
+            record = dict(zip(headers,row))
+        else:
+        # Make tuples
+            record = tuple(row)
             
-            if headers:
-            # Make dictionaries
-                record = dict(zip(headers,row))
-            else:
-            # Make tuples
-                record = tuple(row)
-            
-            records.append(record)
+        records.append(record)
     
     return records
 
